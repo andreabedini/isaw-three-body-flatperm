ac6743194269f5847bbed3ddd09219595c1e9ffc
Andrea Bedini <abedini@ms.unimelb.edu.au>
2014-12-19 13:04:12 +1100

    Don't consider endpoints in NN pairs

---
+++ README.md
@@ -0,0 +1,7 @@
+Notes
+=====
+
+ - Endpoint must not count toward nn pairs! DONE
+ - Still doesn't do what I want, NN interaction is different from
+   N2 (faces touched by two non-consecutive parts of the walk)
+
--- nearest_neighbour.hpp
+++ nearest_neighbour.hpp
@@ -19,25 +19,33 @@ namespace features {
     template<typename Walk>
     void register_step(Walk const& walk)
     {
-      // fetch the last point
-      auto const& x = walk.back();
+      // we need at least three points, i.e. two steps
+      if (walk.size() < 2)
+        return;
+
+      const auto x = walk[walk.size() - 1];
+      const auto origin = Walk::lattice_type::origin();
 
       // iterate over x's neighbours looking for new contacts
       for (auto const& y : Walk::lattice_type::get_neighbours(x)) {
-        if (walk.has_point(y) and not walk.has_bond(x, y))
+        if (y != origin and walk.has_point(y) and not walk.has_bond(x, y)) {
           _m += 1;
+        }
       }
     }
 
     template<typename Walk>
     void unregister_step(Walk const& walk)
     {
-      // fetch the last point
-      auto const& x = walk.back();
+      if (walk.size() < 2)
+        return;
+
+      const auto x = walk[walk.size() - 1];
+      const auto origin = Walk::lattice_type::origin();
 
       // iterate over x's neighbours
       for (auto const& y : Walk::lattice_type::get_neighbours(x)) {
-        if (walk.has_point(y) and not walk.has_bond(x, y))
+        if (y != origin and walk.has_point(y) and not walk.has_bond(x, y))
           _m -= 1;
       }
     }
